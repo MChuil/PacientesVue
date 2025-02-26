59eae37a616d88103e356dcaaad6b16f0e3b7d7e
Miguel Chuil <developer.chuil@gmail.com>
2025-02-26 17:43:51 -0600

    parte final del proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,104 @@
 <script setup>
-  import Header from './components/Header.vue'
-  import Formulario from './components/Formulario.vue' 
+  import { ref, reactive, onMounted, watch } from "vue";
+  import { uid } from "uid";
+  import Header from "./components/Header.vue";
+  import Formulario from "./components/Formulario.vue";
+  import Paciente from "./components/Paciente.vue";
+
+  const pacientes = ref([]);
+
+  const paciente = reactive({
+    id: null,
+    nombre: "",
+    edad: "",
+    email: "",
+    telefono: "",
+    fecha: "",
+    sintomas: "",
+  });
+
+  onMounted(() => {
+    if (localStorage.getItem("pacientes")) {
+      pacientes.value = JSON.parse(localStorage.getItem("pacientes"));
+    }
+  });
+
+  watch((pacientes), () => {
+      guardarPacientes();
+  },{
+      deep: true
+  });
+
+  const guardarPacientes = () => {
+    localStorage.setItem("pacientes", JSON.stringify(pacientes.value));
+  };
+
+  const agregarPaciente = () => {
+    if (paciente.id) { //edición
+      const index = pacientes.value.findIndex( pacienteState => pacienteState.id === paciente.id);
+      pacientes.value[index] = { ...paciente };
+      paciente.id = null;
+    } else {
+      pacientes.value.push({ ...paciente, id: uid() });
+    }
+
+    paciente.nombre = "";
+    paciente.edad = "";
+    paciente.email = "";
+    paciente.telefono = "";
+    paciente.fecha = "";
+    paciente.sintomas = "";
+    paciente.id = null;
+  };
+
+  const actualizarPaciente = (id) => {
+    const pacienteEditar = pacientes.value.filter( (paciente) => paciente.id === id)[0];
+    Object.assign(paciente, pacienteEditar);
+  };
+
+
+  const eliminarPaciente = (id) => {
+    pacientes.value = pacientes.value.filter( (paciente) => paciente.id != id)
+  };
+  
+
 </script>
 
 <template>
   <div class="container mx-auto mt-30">
     <Header />
 
-    <div class="mt-12" md:flex>
-      <Formulario />
+    <div class="mt-12 md:flex">
+      <Formulario
+        v-model:nombre="paciente.nombre"
+        v-model:edad="paciente.edad"
+        v-model:email="paciente.email"
+        v-model:telefono="paciente.telefono"
+        v-model:fecha="paciente.fecha"
+        v-model:sintomas="paciente.sintomas"
+        @guardar-paciente="agregarPaciente"
+        :id = "paciente.id"
+      />
+
+      <div class="md:w-1/2 md:h-screen overflow-y-scroll">
+        <h3 class="font-black text-3xl text-center">
+          Administra tus Pacientes
+        </h3>
+        <div v-if="pacientes.length > 0">
+          <p class="text-lg mt-4 text-center mb-10">
+            Información de
+            <span class="text-lime-600 font-bold">Pacientes</span>
+          </p>
+          <Paciente
+            v-for="paciente in pacientes"
+            :paciente="paciente"
+            @eliminar-paciente="eliminarPaciente"
+            @actualizar-paciente="actualizarPaciente"
+          />
+        </div>
+
+        <p v-else class="mt-20 text-2xl text-center">No hay pacientes</p>
+      </div>
     </div>
   </div>
 </template>
-
--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,5 +1,76 @@
 <script setup>
+    import {reactive} from 'vue';
+    import Alerta from './Alerta.vue';
 
+    const alerta = reactive({
+        titulo: '',
+        mensaje: '',
+        color: ''
+    });
+
+    const emit = defineEmits([
+        'update:nombre',
+        'update:edad',
+        'update:email',
+        'update:telefono',
+        'update:fecha',
+        'update:sintomas',
+        'guardar-paciente',
+        'id'
+    ]);
+
+
+    const props = defineProps({
+        nombre: {
+            type: String,
+            required: true
+        },
+        edad: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        telefono: {
+            type: String,
+            required: true
+        },
+        fecha: {
+            type: String,
+            required: true
+        },
+        sintomas: {
+            type: String,
+            required: true
+        },
+        id:{
+            type: Number,
+            required: false
+        }
+    });
+    
+
+    const validar = e => {
+        if(Object.values(props).includes('')){
+            alerta.titulo = 'Atención';
+            alerta.mensaje = 'Todos los campos son obligatorios';
+            alerta.color = 'red';
+            return;
+        }
+
+        emit('guardar-paciente');
+        sintomas.value = '';
+        alerta.titulo = 'Paciente registrado';
+        alerta.mensaje = 'El paciente se ha registrado correctamente';
+        alerta.color = 'green';
+        setTimeout(() => {
+            alerta.titulo = '';
+            alerta.mensaje = '';
+            alerta.color = '';
+        }, 3000);
+    }
 </script>
 
 <template>
@@ -10,27 +81,57 @@
             Añada Pacientes y 
             <span class="text-lime-600 font-bold">Administralos</span>
         </p>
-
-        <form 
-            class="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
-            action="">
+        <form class="bg-white shadow-md rounded-lg py-10 px-5 mb-10" @submit.prevent="validar">
+            <Alerta 
+                v-if="alerta.mensaje"
+                :alerta="alerta" 
+            />
+            <div class="mb-5">
+                <label for="nombre" class="block text-gray uppercase font-bold">Nombre del paciente</label>
+                <input type="text" id="nombre" placeholder="Nombre del paciente" class="w-full p-2 border-2 placeholder-gray-400 rounded-md border-gray-200"
+                :value = "nombre"
+                @input ="$emit('update:nombre', $event.target.value)"
+                />
+            </div>
+            <div class="mb-5">
+                <label for="edad" class="block text-gray uppercase font-bold">Edad</label>
+                <input type="number" id="edad" placeholder="Edad del paciente" class="w-full p-2 border-2 placeholder-gray-400 rounded-md border-gray-200"
+                :value = "edad"
+                @input ="$emit('update:edad', $event.target.value)"
+                />
+            </div>
+            <div class="mb-5">
+                <label for="email" class="block text-gray uppercase font-bold">Correo electronico</label>
+                <input type="email" id="email" placeholder="Correo del paciente" class="w-full p-2 border-2 placeholder-gray-400 rounded-md border-gray-200"
+                :value = "email"
+                @input ="$emit('update:email', $event.target.value)"
+                />
+            </div>
+            <div class="mb-5">
+                <label for="telefono" class="block text-gray uppercase font-bold">Telefono</label>
+                <input type="number" id="telefono" placeholder="Telefono del paciente" class="w-full p-2 border-2 placeholder-gray-400 rounded-md border-gray-200"
+                :value = "telefono"
+                @input ="$emit('update:telefono', $event.target.value)"
+                />
+            </div>
+            <div class="mb-5">
+                <label for="fecha" class="block text-gray uppercase font-bold">Fecha</label>
+                <input type="date" id="fecha" class="w-full p-2 border-2 placeholder-gray-400 rounded-md border-gray-200"
+                :value = "fecha"
+                @input ="$emit('update:fecha', $event.target.value)"
+                />
+            </div>
                 <div class="mb-5">
-                    <label for="paciente" class="block text-gray uppercase font-bold">Nombre del paciente</label>
-                    <input type="text" id="paciente" placeholder="Nombre del paciente" class="w-full p-2 border-2 placeholder-gray-400 rounded-md border-gray-200" />
+                    <label for="sintomas" class="block text-gray uppercase font-bold">Sintomas o causa de la consulta</label>
+                    <textarea 
+                        id="sintomas" 
+                        class="w-full p-2 border-2 placeholder-gray-400 rounded-md border-gray-200 h-45"
+                        :value = "sintomas"
+                        @input ="$emit('update:sintomas', $event.target.value)"
+                        palceholder="Describe los sintomas" 
+                    ></textarea>
                 </div>
-                <div class="mb-5">
-                    <label for="edad" class="block text-gray uppercase font-bold">Edad</label>
-                    <input type="number" id="edad" placeholder="Edad del paciente" class="w-full p-2 border-2 placeholder-gray-400 rounded-md border-gray-200" />
-                </div>
-                <div class="mb-5">
-                    <label for="email" class="block text-gray uppercase font-bold">Correo electronico</label>
-                    <input type="email" id="email" placeholder="Correo del paciente" class="w-full p-2 border-2 placeholder-gray-400 rounded-md border-gray-200" />
-                </div>
-                <div class="mb-5">
-                    <label for="telefono" class="block text-gray uppercase font-bold">Telefono</label>
-                    <input type="text" id="telefono" placeholder="Telefono del paciente" class="w-full p-2 border-2 placeholder-gray-400 rounded-md border-gray-200" />
-                </div>
-                
+                <button type="sumbit"class="bg-lime-600 text-center text-white uppercase font-bold py-2 px-5 rounded-md w-full hover:bg-lime-800 cursor-pointer transition-color">{{ (id) ? 'Actualizar paciente': 'Registrar paciente' }}</button>
         </form>
     </div>
 </template>
